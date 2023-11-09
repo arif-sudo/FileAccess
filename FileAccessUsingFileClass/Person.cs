76cc17c2f739a47e764c17488fbb1e4010403756
Arif <arif520gg@gmail.com>
2023-11-09 21:44:35 +0400

    Add project files.

---
+++ FileAccessUsingFileClass/Person.cs
@@ -0,0 +1,22 @@
+﻿internal class Person
+{
+    public String Name { get; set; }
+    public String LastName { get; set; }
+    public String Email { get; set; }
+    public String Username { get; set; }
+    public String Password { get; set; }
+    public Person(string name, string lastname, string email)
+    {
+        Name = name;
+        LastName = lastname;
+        Email = email;
+    }
+    public Person(string name, string lastname, string email, string username, string password)
+    {
+        Name = name;
+        LastName = lastname;
+        Email = email;
+        Username = username;
+        Password = password;
+    }
+}
---
+++ FileAccessUsingFileClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string filePath = @"C:\Users\USER\Desktop\file.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+        string[] items = new string[5];
+
+        List<Person> people = new List<Person>();
+        //Person[] people1 = new Person[3];
+        foreach (string line in lines)
+        {
+            items = line.Split(",");
+            Person p = new Person(items[0], items[1], items[2]);
+
+            p.Username = items[3];
+            p.Password = encrypt(items[4]);
+
+            people.Add(p);
+        }
+        
+
+        foreach (Person s in people)
+        {
+            Console.WriteLine(s.Name + " " + s.LastName + " " + s.Email + " " + s.Username + " " + s.Password);
+        }
+
+    }
+    public static String encrypt(String password)
+    {
+        var hashedPassword = "";
+        foreach (char c in password)
+        {
+            hashedPassword += Char.ToString((char)(c + 5));
+        }
+        return hashedPassword;
+    }
+
+}
